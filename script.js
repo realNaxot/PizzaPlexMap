7cd205f50afa40b4d5efeba952541ff5b98fad00
Naxotek <ogorekleon@gmail.com>
2022-06-06 23:50:34 +0200

    firstCommit

---
+++ script.js
@@ -0,0 +1,9 @@
+function Next() {
+  document.getElementById("first-map").style.display = "none";
+  document.getElementById("second-map").style.display = "block";
+}
+
+function Prev() {
+  document.getElementById("second-map").style.display = "none";
+  document.getElementById("first-map").style.display = "block";
+}
